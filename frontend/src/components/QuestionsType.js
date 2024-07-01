fd6f5592bfa57a4a4f3e8f48d9b697ba9b1266d1
sirine kahweji <sirinekahweji5@gmail.com>
2024-07-01 16:12:38 +0200

    Traduction QuestionsType

--- frontend/src/components/QuestionsType.js
+++ frontend/src/components/QuestionsType.js
@@ -1,53 +1,47 @@
+import React, { useContext } from 'react';
 import FormCheckInput from "react-bootstrap/esm/FormCheckInput";
-import './QuestionType.css'; 
-import React from 'react';
+import './QuestionType.css';
+import { LangContext } from '../context/LangContext';
 
 const QuestionsType = ({ onGenerateQuiz }) => {
+    const { currentLangData } = useContext(LangContext);
 
     const handleGenerateQuiz = () => {
         onGenerateQuiz(); // Appeler la fonction pour passer à l'étape Questions
     };
 
-    return ( 
+    return (
         <div className="questionType">
-            <h3>What type of content would you like to create?</h3>
+            <h3>{currentLangData.questionsType.title}</h3>
             <div className="questionsType-container">
                 <div className="type1">
                     <FormCheckInput className="formCheck"></FormCheckInput>
                     <div className="type1-container">
-                        <h5><i class="bi bi-list-check"></i>  Multiple choice questions</h5>
-                        <p>Generate MCQ statements, their correct answers and  associated distractors</p>
+                        <h5><i className="bi bi-list-check"></i> {currentLangData.questionsType.multipleChoice.title}</h5>
+                        <p>{currentLangData.questionsType.multipleChoice.description}</p>
                         <div className="Answers">
-                        <div className="corrcetAnswers">
-                            <p>Number of correct answers*</p>
-                            <input className="input" type="number"></input>
+                            <div className="correctAnswers">
+                                <p>{currentLangData.questionsType.multipleChoice.correctAnswers}</p>
+                                <input className="input" type="number"></input>
+                            </div>
+                            <div className="falseAnswers">
+                                <p>{currentLangData.questionsType.multipleChoice.falseAnswers}</p>
+                                <input className="input" type="number"></input>
+                            </div>
                         </div>
-                        <div className="Answersfalse">
-                            <p>Number of false answers*</p>
-                            <input className="input" type="number"></input>
-                        </div>
-
-                        </div>
-                      
                     </div>
-
                 </div>
                 <div className="type2">
                     <FormCheckInput className="formCheck"></FormCheckInput>
                     <div className="type2-container">
-                       <h5><i class="bi bi-chat-square-fill"></i>  Open questions</h5>
-                       <p>Generate open-ended question statements and associated correct answers</p>
+                        <h5><i className="bi bi-chat-square-fill"></i> {currentLangData.questionsType.openQuestions.title}</h5>
+                        <p>{currentLangData.questionsType.openQuestions.description}</p>
                     </div>
-
-
                 </div>
-
             </div>
-            <button className="generer" onClick={handleGenerateQuiz}>Générer Quiz</button>
-
-
+            <button className="generate" onClick={handleGenerateQuiz}>{currentLangData.questionsType.generateQuizButton}</button>
         </div>
-     );
+    );
 }
- 
+
 export default QuestionsType;
