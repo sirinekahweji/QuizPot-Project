ecfc2fc8667cc4a23d2b733882a22a01a233f1d9
sirine kahweji <sirinekahweji5@gmail.com>
2024-07-11 01:06:56 +0200

    affichage et calcul de score et export des questions

--- frontend/src/components/QCMType.js
+++ frontend/src/components/QCMType.js
@@ -1,38 +1,67 @@
+import React, { useContext, useState } from 'react';
 import FormCheckInput from 'react-bootstrap/esm/FormCheckInput';
 import './QCMType.css';
-import { useContext } from 'react';
 import { LangContext } from '../context/LangContext';
 import { QuestionsContext } from '../context/QuestionsContext';
+import { BiCheckCircle, BiXCircle } from 'react-icons/bi'; // Import icons
 
-
-const QCMType = () => {
+const QCMType = ({ handleScoreUpdate }) => {
     const { currentLangData } = useContext(LangContext);
     const { questions } = useContext(QuestionsContext);
+    const [selectedChoices, setSelectedChoices] = useState({});
 
-    return ( 
-        <div className='listQuestionsQCM' > 
-        <p className='titleList'> <i class="bi bi-list-check"></i> {currentLangData.mcqType.text}</p>
-    
-      {questions && questions.map((question, questionIndex) => (
-        <div className='quizexemple' key={questionIndex}>
-          <div className='quizdiv'>
-            <p className='quiz'>{question.question}</p>
-          </div>
-          <div className='responcesdiv'>
-            {question.choices.map((choice, choiceIndex) => (
-              <div className="choicediv" key={choiceIndex}>
-                <FormCheckInput className="checkbox"></FormCheckInput>
-                <p className='choice-content'>{choice}</p>
-              </div>
+    const handleChoiceSelect = (questionIndex, choiceIndex, isCorrect) => {
+        if (selectedChoices[questionIndex]) {
+            return;
+        }
+
+        setSelectedChoices(prevState => ({
+            ...prevState,
+            [questionIndex]: {
+                choiceIndex,
+                isCorrect
+            }
+        }));
+
+        if (isCorrect) {
+            handleScoreUpdate(prevScore => prevScore + 1); // Mettre à jour le score dans le composant parent
+        }
+    };
+
+    return (
+        <div className='listQuestionsQCM'>
+            <p className='titleList'><i className="bi bi-list-check"></i> {currentLangData.mcqType.text}</p>
+
+            {questions && questions.map((question, questionIndex) => (
+                <div className='quizexemple' key={questionIndex}>
+                    <div className='quizdiv'>
+                        <p className='quiz'>{question.question}</p>
+                    </div>
+                    <div className='responcesdiv'>
+                        {question.choices.map((choice, choiceIndex) => {
+                            const correctAnswer = question.answer.split(') ')[1]; 
+                            const isCorrect = correctAnswer === choice;
+                            const isSelected = selectedChoices[questionIndex] && selectedChoices[questionIndex].choiceIndex === choiceIndex;
+                            const icon = isSelected ? (selectedChoices[questionIndex].isCorrect ? <BiCheckCircle className="icon-correct" /> : <BiXCircle className="icon-incorrect" />) : null;
+
+                            return (
+                                <div className="choicediv" key={choiceIndex} onClick={() => handleChoiceSelect(questionIndex, choiceIndex, isCorrect)}>
+                                    <div className="choice-wrapper">
+                                        <FormCheckInput className="checkbox" checked={isSelected} readOnly />
+                                    </div>
+                                    <p className='choice-content'>{choice}</p>
+                                    {icon}
+                                </div>
+                            );
+                        })}
+                    </div>
+                    <hr />
+                </div>
             ))}
-          </div>
-          <hr />
+           
+            <button className="more"><i className="bi bi-arrow-clockwise"></i> {currentLangData.openQType.generateMore}</button>
         </div>
-      ))}
-        <button className="more"> <i class="bi bi-arrow-clockwise"></i>   {currentLangData.openQType.generateMore}</button>
-
-    </div>
     );
-}
- 
+};
+
 export default QCMType;
--- frontend/src/components/Questions.js
+++ frontend/src/components/Questions.js
@@ -4,18 +4,59 @@ import { LangContext } from '../context/LangContext';
 import OpenQType from './OpenQType';
 import QCMType from './QCMType';
 import axios from 'axios';
+import jsPDF from 'jspdf';
 import { useAuthContext } from '../Hooks/useAuthContext';
 import { QuestionsContext } from '../context/QuestionsContext';
-
 
 const Questions = () => {
     const [selectedType, setSelectedType] = useState('MCQ'); // État pour suivre la sélection
     const { currentLangData } = useContext(LangContext);
     const { questions } = useContext(QuestionsContext);
     const { user } = useAuthContext();
-    const [error, setError] = useState(null);
+    const [score, setScore] = useState(null);
+    const [error, setError] = useState(null); 
 
 
+    const generatePDF = (qs) => {
+        const doc = new jsPDF();
+    
+        let yPos = 10;
+        const margin = 10;
+        const lineHeight = 7;
+    
+        qs.forEach((q, index) => {
+            // Question
+            doc.setFont('helvetica', 'bold');
+            doc.setFontSize(12);
+            doc.text(`Question ${index + 1}: ${q.question}`, margin, yPos);
+            yPos += lineHeight;
+    
+            // Choices
+            doc.setFont('helvetica', 'normal');
+            doc.setFontSize(10);
+            q.choices.forEach((choice, choiceIndex) => {
+                doc.text(`${String.fromCharCode(97 + choiceIndex)}) ${choice}`, margin + 10, yPos);
+                yPos += lineHeight;
+            });
+    
+            // Answer
+            doc.setFont('helvetica', 'italic');
+            doc.text(`Answer: ${q.answer}`, margin, yPos);
+            yPos += lineHeight;
+    
+    
+            // Space between questions
+            yPos += lineHeight;
+        });
+    
+        // Save the PDF
+        doc.save('generated_questions.pdf');
+    };
+
+
+    const handleScoreUpdate = (newScore) => {
+        setScore(newScore); // Mettre à jour le score dans le composant parent
+    };
 
     const savequestions = async (e) => {
         e.preventDefault();
@@ -34,12 +75,41 @@ const Questions = () => {
             console.error('Error  save questions', error);
             setError('Failed to save questions. Please try again later.');
           }
-
     }
 
     const handleSelection = (type) => {
         setSelectedType(type);
     }
+
+    const progressBar = (widthPerc, gradient = false) => {
+        const radius = 65;
+        const dashArray = (Math.PI * radius * widthPerc) / 100;
+        return (
+            <svg width="200" height="120">
+                <circle
+                    cx="100"
+                    cy="100"
+                    r={radius}
+                    fill="none"
+                    strokeWidth="25"
+                    strokeLinecap="round"
+                    strokeDashoffset={-1 * Math.PI * radius}
+                    strokeDasharray={`${dashArray} 10000`} // Utilisation correcte des backticks pour les templates literals
+                    stroke={gradient ? "url(#score-gradient)" : "#e5e5e5"}
+                ></circle>
+                {gradient && (
+                    <defs>
+                        <linearGradient id="score-gradient">
+                            <stop offset="0%" stopColor="red" />
+                            <stop offset="25%" stopColor="orange" />
+                            <stop offset="100%" stopColor="green" />
+                        </linearGradient>
+                    </defs>
+                )}
+            </svg>
+        );
+    };
+    
 
     return ( 
         <div className="questions">
@@ -53,13 +123,27 @@ const Questions = () => {
                     <p onClick={() => handleSelection('Open')} className={selectedType === 'Open' ? 'selected' : ''}>
                         <i className={currentLangData.questions.open.icon}></i> {currentLangData.questions.open.text}
                     </p>
+                    {score !== null && (
+                        <div className="score-wrap">
+                            <div className="score">
+                                <div className="score-bar">
+                                    <div className="placeholder">{progressBar(100)}</div>
+                                    <div className="score-circle">{progressBar((score / questions.length) * 100, true)}</div>
+                                </div>
+                                <div className="score-value">
+                                    <div className="score-name">Score</div>
+                                    <div className="score-number">{Math.round((score / questions.length) * 100)}%</div>
+                                </div>
+                            </div>
+                        </div>
+                    )}
                 </div>
-                {selectedType === 'MCQ' && <QCMType />}
+                {selectedType === 'MCQ' && <QCMType handleScoreUpdate={handleScoreUpdate} />}
                 {selectedType === 'Open' && <OpenQType />}
             </div>
             <div className='btns'> 
-                <button className='btn'><i className="bi bi-download"></i>   {currentLangData.questions.buttons.export} </button>
-                <button className='btn' onClick={savequestions}>{currentLangData.questions.buttons.save} <i class="bi bi-chevron-right"></i></button>
+                <button className='btn' onClick={() => generatePDF(questions)}><i className="bi bi-download"></i>   {currentLangData.questions.buttons.export} </button>
+                <button className='btn' onClick={savequestions}>{currentLangData.questions.buttons.save} <i className="bi bi-chevron-right"></i></button>
             </div>
         </div>
     );
