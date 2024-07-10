1a19ee74b63e1434ba3c37be5409f154b31c8c64
sirine kahweji <sirinekahweji5@gmail.com>
2024-07-10 16:54:20 +0200

    security config

--- gemini.js
+++ gemini.js
@@ -1,14 +1,13 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 // Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI('AIzaSyByCX2MbBweVqrm8Fjw63ZmzLHJVQZ6IPo');
+const genAI = new GoogleGenerativeAI(process.env.KEY);
 
 async function run(test) {
    try{
   // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
-  //const prompt = "Generate 5 questions on the topic 'Mathematic' at high school level with medium difficulty. Focus areas: Algebra, Geometry. Include both QCM and Open questions."
 
   const result = await model.generateContent(test);
   const response = await result.response;
@@ -19,20 +18,7 @@ async function run(test) {
   if (!text) {
     throw new Error('Empty response received from Gemini.');
   }
-  // Split the generatedContent into structured parts
- /* const [title, subject, grade, time, objectives, materials, procedure, assessment] = generatedContent.split("\n\n");
-
-  // Return a JSON object with structured data
-  return {
-    title: title.trim(),
-    subject: subject.trim(),
-    grade: grade.trim(),
-    time: time.trim(),
-    objectives: objectives.trim(),
-    materials: materials.trim(),
-    procedure: procedure.trim(),
-    assessment: assessment.trim(),
-  };*/
+  
 
   
 }
