b65217106227a86f89d19026aaa2b4fc79827237
sirine kahweji <sirinekahweji5@gmail.com>
2024-07-11 18:24:43 +0200

    afficher user response

--- backend/controllers/responseformController.js
+++ backend/controllers/responseformController.js
@@ -42,6 +42,33 @@ const saveResponseForm = async (req, res) => {
   }
 };
 
+
+const getResponseForms = async (req, res) => {
+
+    const userId = req.user._id;
+   console.log(userId)
+    if (!userId) {
+      return res.status(400).json({ error: 'User ID is required' });
+    }
+  
+    try {
+      if (!mongoose.Types.ObjectId.isValid(userId)) {
+        return res.status(400).json({ error: 'Invalid user ID' });
+      }
+  
+      const userExists = await User.findById(userId);
+      if (!userExists) {
+        return res.status(404).json({ error: 'User not found' });
+      }
+  
+      const responseForms = await ResponseForm.find({ userId });
+  
+      res.status(200).json(responseForms);
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to retrieve response forms' });
+    }
+  };
+
 module.exports = {
-  saveResponseForm,
+  saveResponseForm,getResponseForms
 };
--- backend/routes/responseform.js
+++ backend/routes/responseform.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { saveResponseForm } = require('../controllers/responseformController');
+const { saveResponseForm ,getResponseForms} = require('../controllers/responseformController');
 const requireAuth = require('../middleware/requireAuth');
 
 router.post('/save',requireAuth ,saveResponseForm);
+router.get('/',requireAuth ,getResponseForms);
 
 module.exports = router;
