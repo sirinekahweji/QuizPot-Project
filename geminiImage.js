d737cd1ff330dd30ed2202952a9b1e296423dddc
sirine kahweji <sirinekahweji5@gmail.com>
2024-07-15 23:38:12 +0200

    generate text from image with gemini

---
+++ geminiImage.js
@@ -0,0 +1,32 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const fs = require("fs");
+
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI('AIzaSyByCX2MbBweVqrm8Fjw63ZmzLHJVQZ6IPo');
+
+// Converts local file information to a GoogleGenerativeAI.Part object.
+function fileToGenerativePart(path, mimeType) {
+  return {
+    inlineData: {
+      data: Buffer.from(fs.readFileSync(path)).toString("base64"),
+      mimeType
+    },
+  };
+}
+
+async function run() {
+  // The Gemini 1.5 models are versatile and work with both text-only and multimodal prompts
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const prompt = "generate 6 questions from image1 chque questions a 3 choix et un seule correcte answer?";
+  const imageParts = [
+    fileToGenerativePart("image1.jpg", "image/png"),
+    fileToGenerativePart("image2.jpg", "image/jpeg"),
+  ];
+
+  const result = await model.generateContent([prompt, ...imageParts]);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+}
+
+run();
