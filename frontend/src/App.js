470ee1feb15a3ac2ff22cd2b6b84b2641436303e
sirine kahweji <sirinekahweji5@gmail.com>
2024-07-26 01:26:38 +0200

    dernier commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,14 @@ import{BrowserRouter,Routes,Route,Navigate} from 'react-router-dom'
 import PageGarde from './pages/PageGarde';
 import Home from './pages/Home';
 import ResetPassword from './pages/ResetPassword';
+import Dashbord from './pages/Dashbord';
 import Profile from './pages/Profile';
 import NavBar from './components/NavBar';
 import './index.css'; 
 import './'
 import { LangProvider } from './context/LangContext';
 import { FormDataProvider } from './context/FormDataContext';
-
 import { QuestionsProvider } from './context/QuestionsContext';
-
 import SignUp from './pages/SignUp';
 import { useAuthContext } from "./Hooks/useAuthContext";
 
@@ -32,6 +31,7 @@ function App() {
             <Route path="/" element={!user ? <PageGarde/>:<Navigate to='/quizbot'/>  } />
             <Route path="/signup" element={!user ? <SignUp/> : <Navigate to='/quizbot'/> } />
             <Route path="/quizbot" element={user ? <Home/> :<Navigate to='/'/> } />
+            <Route path="/dashbord" element={user ? <Dashbord/> :<Navigate to='/'/> } />
             <Route path="/profile" element={user ? <Profile/> :<Navigate to='/'/> } />
             <Route path="/ResetPassword" element={!user ? <ResetPassword/> :<Navigate to='/quizbot'/> } />
          </Routes>
---
+++ frontend/src/pages/Dashbord.js
@@ -0,0 +1,10 @@
+import './Dashbord.css'
+const Dashbord = () => {
+    return ( 
+        <div>
+
+        </div>
+     );
+}
+ 
+export default Dashbord;
