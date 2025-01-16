dac98d5f1d48c889b5fd5d84b201f849ce273525
sirine kahweji <sirinekahweji5@gmail.com>
2025-01-16 13:54:25 +0100

    add captures

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,6 @@ const responseFormRoutes = require('./routes/responseform');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.static('public'));
-
 
 // Middleware
 app.use(express.json());
